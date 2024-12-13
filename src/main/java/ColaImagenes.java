72c488bd1dc194a6bd1ce650727f61f85e0ca4c4
dariogithub24 <dario.marcelo.z@gmail.com>
2024-12-13 19:55:10 -0300

    eliminación del semaforo en colaImagenes

--- src/main/java/ColaImagenes.java
+++ src/main/java/ColaImagenes.java
@@ -1,47 +1,34 @@
 import java.util.ArrayList;
-import java.util.concurrent.Semaphore;
 
-/*
-* Clase Cola
-*
-* lista: Array de imagenes
-* mutex: Semaforo para sincronizar el acceso a la cola
-*/
+
+
 public class ColaImagenes {
   private ArrayList<Imagen> lista;
-  private Semaphore mutex;
-
+ 
+  
   // constructor
   public ColaImagenes(){
     lista = new ArrayList<Imagen>();
-    mutex = new Semaphore(1);
+    
   }
 
   //Método para agregar una imagen a la cola
   public void encolar(Imagen img, int transicion) {
-    try {
-      mutex.acquire(); //acquire del semaforo
-    } catch(InterruptedException e) {
-      System.out.println("error al encolar");
-    }
+     
     img.agregarTransicion(transicion);
     lista.add(img); //agrega a la lista
-    mutex.release(); //release del semaforo
+     
   }
   
   //Método para sacar una imagen de la cola
   public Imagen desencolar() {
-    try{
-      mutex.acquire();  //adquiere el semaforo
-    } catch(InterruptedException e) {
-      System.out.println("error al desencolar");
-    }
+     
     Imagen img = null;
     if(!lista.isEmpty()) {  // si la lista no esta vacia toma la imagen y la saca de la misma
       img = lista.get(0);
       lista.remove(0);
     }
-    mutex.release(); // release del semaforo
+     
     return img;
   }
 
